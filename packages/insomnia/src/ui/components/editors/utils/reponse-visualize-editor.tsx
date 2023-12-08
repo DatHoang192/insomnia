eecc60b1058c6f9e64b208b813596c7933348a56
huuphat1908 <huuphat1908@gmail.com>
2023-12-08 03:43:57 +0000

    DEV-003 remove show raw tag checkbox

--- packages/insomnia/src/ui/components/editors/utils/reponse-visualize-editor.tsx
+++ packages/insomnia/src/ui/components/editors/utils/reponse-visualize-editor.tsx
@@ -52,15 +52,6 @@ export const ResponseVisualizeEditor = () => {
   const patchRequest = useRequestPatcher();
   const uniqueKey = `${activeRequest._id}::response-visualizer-setting`;
 
-  console.log(activeRequestMeta.visualizeTemplate);
-
-  const handleUpdateNunjucksPowerUserMode = useCallback(
-    (value: boolean) => {
-      patchRequestMeta(activeRequest._id, { visualizePowerUserMode: !value });
-    },
-    [activeRequest._id, patchRequestMeta]
-  );
-
   const handleUpdateVisualization = useCallback(
     (value: boolean) => {
       patchRequest(activeRequest._id, { settingResponseVisualize: !value });
@@ -79,11 +70,6 @@ export const ResponseVisualizeEditor = () => {
     <Container>
       <div className="editor-header">
         <h4>Response visualize editor</h4>
-        <Checkbox
-          checked={activeRequestMeta.visualizePowerUserMode || false}
-          label={'Show raw tag'}
-          onChange={handleUpdateNunjucksPowerUserMode}
-        />
         <Checkbox
           checked={activeRequest.settingResponseVisualize || false}
           label={'Enable visualization'}
