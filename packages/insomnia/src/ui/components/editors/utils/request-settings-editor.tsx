c90ca8dee9739d7270359e961edd3a2813960bb3
DatHoang192 <tiendat.hoang192@gmail.com>
2024-01-09 13:13:19 +0700

    DEV084 setting in utils tab

--- packages/insomnia/src/ui/components/editors/utils/request-settings-editor.tsx
+++ packages/insomnia/src/ui/components/editors/utils/request-settings-editor.tsx
@@ -1,5 +1,442 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import {
+  useFetcher,
+  useNavigate,
+  useParams,
+  useRouteLoaderData,
+} from 'react-router-dom';
+
+import { request } from '../../../../models';
+import { isGrpcRequest } from '../../../../models/grpc-request';
+import { isRequest, Request } from '../../../../models/request';
+import { isWebSocketRequest } from '../../../../models/websocket-request';
+import { invariant } from '../../../../utils/invariant';
+import { useRequestPatcher } from '../../../hooks/use-request';
+import { ProjectLoaderData } from '../../../routes/project';
+import { RequestLoaderData } from '../../../routes/request';
+import { CodeEditorHandle } from '../../codemirror/code-editor';
+import { HelpTooltip } from '../../help-tooltip';
+import { MarkdownEditor } from '../../markdown-editor';
+
+interface State {
+  defaultPreviewMode: boolean;
+  activeWorkspaceIdToCopyTo: string;
+}
 
 export const RequestSettingsEditor = () => {
-  return <div>Setting</div>;
+  const editorRef = useRef<CodeEditorHandle>(null);
+
+  const { activeRequest } = useRouteLoaderData(
+    'request/:requestId'
+  ) as RequestLoaderData;
+
+  const { organizationId, projectId, workspaceId } = useParams() as {
+    organizationId: string;
+    projectId: string;
+    workspaceId: string;
+  };
+  const patchRequest = useRequestPatcher();
+  const workspacesFetcher = useFetcher();
+  const navigate = useNavigate();
+  const requestFetcher = useFetcher();
+
+  const duplicateRequest = (r: Partial<Request>) => {
+    requestFetcher.submit(JSON.stringify(r), {
+      action: `/organization/${organizationId}/project/${projectId}/workspace/${workspaceId}/debug/request/${activeRequest._id}/duplicate`,
+      method: 'post',
+      encType: 'application/json',
+    });
+  };
+
+  const [state, setState] = useState<State>({
+    defaultPreviewMode: !!activeRequest,
+    activeWorkspaceIdToCopyTo: '',
+  });
+
+  const { defaultPreviewMode, activeWorkspaceIdToCopyTo } = state;
+
+  const toggleCheckBox = async (event: any) => {
+    patchRequest(activeRequest._id, {
+      [event.currentTarget.name]: !!event.currentTarget.checked,
+    });
+  };
+
+  const projectLoaderData = workspacesFetcher?.data as ProjectLoaderData;
+  const workspacesForActiveProject =
+    projectLoaderData?.workspaces.map(w => w.workspace) || [];
+
+  async function handleMoveToWorkspace() {
+    invariant(state.activeWorkspaceIdToCopyTo, 'Workspace ID is required');
+    patchRequest(activeRequest._id, {
+      parentId: state.activeWorkspaceIdToCopyTo,
+    });
+    // modalRef.current?.hide();
+    navigate(
+      `/organization/${organizationId}/project/${projectId}/workspace/${state.activeWorkspaceIdToCopyTo}/debug`
+    );
+  }
+
+  async function handleCopyToWorkspace() {
+    invariant(state.activeWorkspaceIdToCopyTo, 'Workspace ID is required');
+    duplicateRequest({ parentId: state.activeWorkspaceIdToCopyTo });
+  }
+
+  const updateDescription = (description: string) => {
+    patchRequest(activeRequest._id, { description });
+    setState({
+      ...state,
+      defaultPreviewMode: false,
+    });
+  };
+
+  return (
+    <div className='pad'>
+      Setting
+      <div>
+        <span className='txt-sm selectable faint monospace'>
+          ID: {activeRequest ? activeRequest._id : ''}
+        </span>
+        <div className='form-control form-control--outlined'>
+          <label>
+            Name{' '}
+            <span className='txt-sm faint italic'>
+              (also rename by double-clicking in sidebar)
+            </span>
+            <input
+              type='text'
+              placeholder={activeRequest?.url || 'My Request'}
+              defaultValue={activeRequest?.name}
+              onChange={event =>
+                patchRequest(activeRequest._id, { name: event.target.value })
+              }
+            />
+          </label>
+        </div>
+        {activeRequest && isWebSocketRequest(activeRequest) && (
+          <>
+            <MarkdownEditor
+              ref={editorRef}
+              className='margin-top'
+              defaultPreviewMode={defaultPreviewMode}
+              placeholder='Write a description'
+              defaultValue={activeRequest.description}
+              onChange={updateDescription}
+            />
+            <>
+              <div className='pad-top pad-bottom'>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Apply filter dataset by active environment
+                    <input
+                      type='checkbox'
+                      name='settingDatasetFilter'
+                      checked={!!activeRequest.settingDatasetFilter}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Using response visualization
+                    <input
+                      type='checkbox'
+                      name='settingResponseVisualize'
+                      checked={!!activeRequest.settingResponseVisualize}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Send cookies automatically
+                    <input
+                      type='checkbox'
+                      name='settingSendCookies'
+                      checked={activeRequest.settingSendCookies}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Store cookies automatically
+                    <input
+                      type='checkbox'
+                      name='settingStoreCookies'
+                      checked={activeRequest.settingStoreCookies}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className='form-control form-control--outlined'>
+                <label>
+                  Follow redirects{' '}
+                  <span className='txt-sm faint italic'>
+                    (overrides global setting)
+                  </span>
+                  <select
+                    defaultValue={activeRequest.settingFollowRedirects}
+                    name='settingFollowRedirects'
+                    onChange={toggleCheckBox}
+                  >
+                    <option value={'global'}>Use global setting</option>
+                    <option value={'off'}>Don't follow redirects</option>
+                    <option value={'on'}>Follow redirects</option>
+                  </select>
+                </label>
+              </div>
+            </>
+            <hr />
+            <div className='form-row'>
+              <div className='form-control form-control--outlined'>
+                <label>
+                  Move/Copy to Workspace
+                  <HelpTooltip position='top' className='space-left'>
+                    Copy or move the current request to a new workspace. It will
+                    be placed at the root of the new workspace's folder
+                    structure.
+                  </HelpTooltip>
+                  <select
+                    value={activeWorkspaceIdToCopyTo}
+                    onChange={event => {
+                      const activeWorkspaceIdToCopyTo =
+                        event.currentTarget.value;
+                      setState(state => ({
+                        ...state,
+                        activeWorkspaceIdToCopyTo,
+                      }));
+                    }}
+                  >
+                    <option value=''>-- Select Workspace --</option>
+                    {workspacesForActiveProject.map(w => {
+                      if (workspaceId === w._id) {
+                        return null;
+                      }
+
+                      return (
+                        <option key={w._id} value={w._id}>
+                          {w.name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </label>
+              </div>
+              <div className='form-control form-control--no-label width-auto'>
+                <button
+                  disabled={!activeWorkspaceIdToCopyTo}
+                  className='btn btn--clicky'
+                  onClick={handleCopyToWorkspace}
+                >
+                  Copy
+                </button>
+              </div>
+              <div className='form-control form-control--no-label width-auto'>
+                <button
+                  disabled={!activeWorkspaceIdToCopyTo}
+                  className='btn btn--clicky'
+                  onClick={handleMoveToWorkspace}
+                >
+                  Move
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+        {activeRequest && isGrpcRequest(activeRequest) && (
+          <p className='faint italic'>
+            Are there any gRPC settings you expect to see? Create a{' '}
+            <a href={'https://github.com/Kong/insomnia/issues/new/choose'}>
+              feature request
+            </a>
+            !
+          </p>
+        )}
+        {activeRequest && isRequest(activeRequest) && (
+          <>
+            <MarkdownEditor
+              ref={editorRef}
+              className='margin-top'
+              defaultPreviewMode={defaultPreviewMode}
+              placeholder='Write a description'
+              defaultValue={activeRequest.description}
+              onChange={updateDescription}
+            />
+            <>
+              <div className='pad-top pad-bottom'>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Apply filter dataset by active environment
+                    <input
+                      type='checkbox'
+                      name='settingDatasetFilter'
+                      checked={!!activeRequest.settingDatasetFilter}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Using response visualization
+                    <input
+                      type='checkbox'
+                      name='settingResponseVisualize'
+                      checked={!!activeRequest.settingResponseVisualize}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Send cookies automatically
+                    <input
+                      type='checkbox'
+                      name='settingSendCookies'
+                      checked={activeRequest.settingSendCookies}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Store cookies automatically
+                    <input
+                      type='checkbox'
+                      name='settingStoreCookies'
+                      checked={activeRequest.settingStoreCookies}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Automatically encode special characters in URL
+                    <input
+                      type='checkbox'
+                      name='settingEncodeUrl'
+                      checked={activeRequest.settingEncodeUrl}
+                      onChange={toggleCheckBox}
+                    />
+                    <HelpTooltip position='top' className='space-left'>
+                      Automatically encode special characters at send time (does
+                      not apply to query parameters editor)
+                    </HelpTooltip>
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Skip rendering of request body
+                    <input
+                      type='checkbox'
+                      name='settingDisableRenderRequestBody'
+                      checked={activeRequest.settingDisableRenderRequestBody}
+                      onChange={toggleCheckBox}
+                    />
+                    <HelpTooltip position='top' className='space-left'>
+                      Disable rendering of environment variables and tags for
+                      the request body
+                    </HelpTooltip>
+                  </label>
+                </div>
+                <div className='form-control form-control--thin'>
+                  <label>
+                    Rebuild path dot sequences
+                    <HelpTooltip position='top' className='space-left'>
+                      This instructs libcurl to squash sequences of "/../" or
+                      "/./" that may exist in the URL's path part and that is
+                      supposed to be removed according to RFC 3986 section 5.2.4
+                    </HelpTooltip>
+                    <input
+                      type='checkbox'
+                      name='settingRebuildPath'
+                      checked={activeRequest['settingRebuildPath']}
+                      onChange={toggleCheckBox}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className='form-control form-control--outlined'>
+                <label>
+                  Follow redirects{' '}
+                  <span className='txt-sm faint italic'>
+                    (overrides global setting)
+                  </span>
+                  <select
+                    defaultValue={activeRequest.settingFollowRedirects}
+                    name='settingFollowRedirects'
+                    onChange={async event => {
+                      const updated = await request.update(activeRequest, {
+                        [event.currentTarget.name]: event.currentTarget.value,
+                      });
+                      setState(state => ({ ...state, request: updated }));
+                    }}
+                  >
+                    <option value={'global'}>Use global setting</option>
+                    <option value={'off'}>Don't follow redirects</option>
+                    <option value={'on'}>Follow redirects</option>
+                  </select>
+                </label>
+              </div>
+            </>
+            <hr />
+            <div className='form-row'>
+              <div className='form-control form-control--outlined'>
+                <label>
+                  Move/Copy to Workspace
+                  <HelpTooltip position='top' className='space-left'>
+                    Copy or move the current request to a new workspace. It will
+                    be placed at the root of the new workspace's folder
+                    structure.
+                  </HelpTooltip>
+                  <select
+                    value={activeWorkspaceIdToCopyTo}
+                    onChange={event => {
+                      const activeWorkspaceIdToCopyTo =
+                        event.currentTarget.value;
+                      setState(state => ({
+                        ...state,
+                        activeWorkspaceIdToCopyTo,
+                      }));
+                    }}
+                  >
+                    <option value=''>-- Select Workspace --</option>
+                    {workspacesForActiveProject.map(w => {
+                      if (workspaceId === w._id) {
+                        return null;
+                      }
+
+                      return (
+                        <option key={w._id} value={w._id}>
+                          {w.name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </label>
+              </div>
+              <div className='form-control form-control--no-label width-auto'>
+                <button
+                  disabled={!activeWorkspaceIdToCopyTo}
+                  className='btn btn--clicky'
+                  onClick={handleCopyToWorkspace}
+                >
+                  Copy
+                </button>
+              </div>
+              <div className='form-control form-control--no-label width-auto'>
+                <button
+                  disabled={!activeWorkspaceIdToCopyTo}
+                  className='btn btn--clicky'
+                  onClick={handleMoveToWorkspace}
+                >
+                  Move
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
