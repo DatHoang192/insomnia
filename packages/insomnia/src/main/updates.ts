db5031673924f5c1c09b8b970f3eaeeec8512ed4
huuphat1908 <huuphat1908@gmail.com>
2024-01-11 07:36:07 +0000

    DEV088 - disable automatic update

--- packages/insomnia/src/main/updates.ts
+++ packages/insomnia/src/main/updates.ts
@@ -1,43 +1,4 @@
-import { autoUpdater, BrowserWindow, dialog, ipcMain } from 'electron';
-
-import {
-  CHECK_FOR_UPDATES_INTERVAL,
-  getAppId,
-  getAppVersion,
-  isDevelopment,
-  UpdateURL,
-} from '../common/constants';
-import { delay } from '../common/misc';
-import * as models from '../models/index';
-import { invariant } from '../utils/invariant';
-const isUpdateSupported = () => {
-  if (process.platform === 'linux') {
-    console.log('[updater] Not supported on this platform', process.platform);
-    return false;
-  }
-  if (process.platform === 'win32' && process.env['PORTABLE_EXECUTABLE_DIR']) {
-    console.log('[updater] Not supported on portable windows binary');
-    return false;
-  }
-  if (process.env.INSOMNIA_DISABLE_AUTOMATIC_UPDATES) {
-    console.log('[updater] Disabled by INSOMNIA_DISABLE_AUTOMATIC_UPDATES environment variable');
-    return false;
-  }
-  if (isDevelopment()) {
-    console.log('[updater] Disabled in dev mode');
-    return false;
-  }
-  return true;
-};
-const getUpdateUrl = (updateChannel: string): string | null => {
-  invariant(isUpdateSupported(), 'auto update is not supported');
-  const fullUrl = new URL(process.platform === 'win32' ? UpdateURL.windows : UpdateURL.mac);
-  fullUrl.searchParams.append('v', getAppVersion());
-  fullUrl.searchParams.append('app', getAppId());
-  fullUrl.searchParams.append('channel', updateChannel);
-  console.log(`[updater] Using url ${fullUrl.toString()}`);
-  return fullUrl.toString();
-};
+import { autoUpdater, BrowserWindow, dialog } from 'electron';
 
 const _sendUpdateStatus = (status: string) => {
   for (const window of BrowserWindow.getAllWindows()) {
@@ -75,47 +36,4 @@ export const init = async () => {
       }
     });
   });
-
-  if (isUpdateSupported()) {
-    // perhaps disable this method of upgrading just incase it trigger before backup is complete
-    // on app start
-    const settings = await models.settings.get();
-    const updateUrl = getUpdateUrl(settings.updateChannel);
-    if (settings.updateAutomatically && updateUrl) {
-      _checkForUpdates(updateUrl);
-    }
-    // on an interval (3h)
-    setInterval(async () => {
-      const settings = await models.settings.get();
-      const updateUrl = getUpdateUrl(settings.updateChannel);
-      if (settings.updateAutomatically && updateUrl) {
-        _checkForUpdates(updateUrl);
-      }
-    }, CHECK_FOR_UPDATES_INTERVAL);
-
-    // on check now button pushed
-    ipcMain.on('manualUpdateCheck', async () => {
-      console.log('[updater] Manual update check');
-      const settings = await models.settings.get();
-      const updateUrl = isUpdateSupported() && getUpdateUrl(settings.updateChannel);
-      if (!updateUrl) {
-        _sendUpdateStatus('Updates Not Supported');
-        return;
-      }
-      _sendUpdateStatus('Checking');
-      await delay(300); // Pacing
-      _checkForUpdates(updateUrl);
-    });
-  }
-};
-
-const _checkForUpdates = (updateUrl: string) => {
-  try {
-    console.log(`[updater] Checking for updates url=${updateUrl}`);
-    autoUpdater.setFeedURL({ url: updateUrl });
-    autoUpdater.checkForUpdates();
-  } catch (err) {
-    console.warn('[updater] Failed to check for updates:', err.message);
-    _sendUpdateStatus('Update Error');
-  }
 };
